deabf889e9507be431b1969eeeabfb7bc9accfcf
Yoad <yoad787@gmail.com>
2024-03-02 15:21:23 +0200

    Update terratest

--- devops_repo/GCP/terratest/stages.go
+++ devops_repo/GCP/terratest/stages.go
@@ -7,16 +7,17 @@ import (
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
-var t *testing.T
 var MyConfigStages = NewConfig()
 
 func deployVpcStage() {
+	var t *testing.T
 	terragruntOptionsVpc := terraform.WithDefaultRetryableErrors(t, configVPC(t, MyConfigStages.TerragruntDirPathVpc))
 	test_structure.SaveTerraformOptions(t, "/tmp", terragruntOptionsVpc)
 	terraform.InitAndApply(t, terragruntOptionsVpc)
 }
 
 func cleanVpcStage() {
+	var t *testing.T
 	terragruntOptionsVpc := test_structure.LoadTerraformOptions(t, "/tmp")
 	terraform.Destroy(t, terragruntOptionsVpc)
 }
